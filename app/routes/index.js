93b1b7e3b30629bb6f897f7094383a7128222751
Rory McLaughlin <rmclaughlin@nectarom.com>
2018-10-19 10:55:10 -0500

    Initial commit.

---
+++ app/routes/index.js
@@ -0,0 +1,23 @@
+'use strict';
+const router = require('express').Router();
+
+module.exports = () => {
+    let routes = {
+        'get': {
+            '/': (req, res, next) => {
+                res.render('login')
+            },
+            '/rooms': (req, res, next) => {
+                res.render('rooms');
+            },
+            '/chat': (req, res, next) => {
+                res.render('chatroom');
+            }
+        },
+        'post': {
+
+        }
+    }
+
+    //Iterate through the routes object and mount the routes
+}
