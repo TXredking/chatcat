7e1dfc35e7cd2f20a9ddeee57a360aadc740b3df
Rory McLaughlin <rmclaughlin@nectarom.com>
2018-10-23 17:39:51 -0500

    Add initial Socket.io setup.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 'use strict';
 const utils = require('../utils');
 const passport = require('passport');
+const config = require('../config');
 
 module.exports = () => {
     let routes = {
@@ -10,12 +11,14 @@ module.exports = () => {
             },
             '/rooms': [utils.isAuthenticated, (req, res, next) => {
                 res.render('rooms', {
-                    user: req.user
+                    user: req.user,
+                    host: config.host
                 });
             }],
             '/chat': [utils.isAuthenticated, (req, res, next) => {
                 res.render('chatroom', {
-                    user: req.user
+                    user: req.user,
+                    host: config.host
                 });
             }],
             '/auth/facebook': passport.authenticate('facebook'),
---
+++ app/socket/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = (io, app) => {
+    let allrooms = app.locals.chatrooms;
+
+    allrooms.push({
+        room: 'Good Food',
+        roomID: '0001',
+        users: []
+    });
+
+    allrooms.push({
+        room: 'Music 101',
+        roomID: '0002',
+        users: []
+    });
+
+    io.of('/roomslist').on('connection', socket => {
+        socket.on('getChatrooms', () => {
+            socket.emit('chatRoomsList', JSON.stringify(allrooms));
+        });
+    });
+}
